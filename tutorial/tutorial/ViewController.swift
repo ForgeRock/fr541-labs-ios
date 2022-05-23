6dbf7596d412a3312dea704a91b5602849f81458
sngeza <geza.simon@dpc.hu>
2022-05-23 12:07:20 +0200

    copyrights

--- tutorial/tutorial/ViewController.swift
+++ tutorial/tutorial/ViewController.swift
@@ -1,9 +1,9 @@
-//
-//  ViewController.swift
-//  tutorial
-//
-//  Created by Geza Simon on 2022. 04. 12..
-//
+/*
+ * Copyright (c) 2022 ForgeRock. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
 
 import UIKit
 //DONE INIT: import FRAuth
